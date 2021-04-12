732767da03853e6df24367a57a3f7c35802501da
LorenitaTL <lore7096@gmail.com>
2021-04-12 12:24:45 -0500

    Navbar responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 
 export const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  console.log(showMenu);
+
+  const handleShowMenu = () => setShowMenu(!showMenu);
   return (
     <nav className="navbar">
-      <p>
-        <Link to="home" smooth={true} duration={1000}>
-          Home
-        </Link>
-      </p>
-      <p>
-        <Link to="about-me" smooth={true} duration={1000}>
-          About me
-        </Link>
-      </p>
-      <p>
-        <Link to="my-projects" smooth={true} duration={1000}>
-          My Projects
-        </Link>
-      </p>
-      <p>
-        <Link to="technologies" smooth={true} duration={1000}>
-          Technologies
-        </Link>
-      </p>
-      <p>
-        <Link to="contact" smooth={true} duration={1000}>
-          Contact me
-        </Link>
-      </p>
+      <div>
+        <div className="button-menu" onClick={handleShowMenu}>
+          <i className="fa fa-bars fa-2x" aria-hidden="true"></i>
+        </div>
+      </div>
+      <div className={`options ${showMenu ? "options" : "hide-options"}`}>
+        <p>
+          <Link
+            to="home"
+            smooth={true}
+            duration={1000}
+            onClick={handleShowMenu}
+          >
+            Home
+          </Link>
+        </p>
+        <p>
+          <Link
+            to="about-me"
+            smooth={true}
+            duration={1000}
+            onClick={handleShowMenu}
+          >
+            About me
+          </Link>
+        </p>
+        <p>
+          <Link
+            to="my-projects"
+            smooth={true}
+            duration={1000}
+            onClick={handleShowMenu}
+          >
+            My Projects
+          </Link>
+        </p>
+        <p>
+          <Link
+            to="technologies"
+            smooth={true}
+            duration={1000}
+            onClick={handleShowMenu}
+          >
+            Technologies
+          </Link>
+        </p>
+        <p>
+          <Link
+            to="contact"
+            smooth={true}
+            duration={1000}
+            onClick={handleShowMenu}
+          >
+            Contact me
+          </Link>
+        </p>
+      </div>
     </nav>
   );
 };
